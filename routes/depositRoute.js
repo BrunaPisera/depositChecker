dd88a6ee312beaea441ab364de62e20fe7b63122
matheus.minguini <matheus.minguini@timwe.com>
2023-03-28 12:27:38 -0300

    Improving your code

--- routes/depositRoute.js
+++ routes/depositRoute.js
@@ -2,13 +2,14 @@ const connectionPool = require('../database/dbConnection')()
 const depositsDAO = require('../database/depositsDAO')()
 
 module.exports = function routes(app){
-    app.get('/deposit', async (request, response) => {
-        console.log(connectionPool)
-        connectionPool.query("SET search_path TO 'DEPOSITS-MS';")
-
-        const rows = await depositsDAO.getAllDeposits(connectionPool)
-
-        console.log('Banana')
-        response.send('Brunaaa')
+    app.get('/deposit', (httpRequest, httpResponse) => {
+        // Dealing with promise
+        depositsDAO.getAllDeposits(connectionPool)
+            .then((result) => {
+                console.log(`Response from Promise: ${result}`)
+                httpResponse.status(200).send(result.rows.length)
+            }).catch((error) => {
+                httpResponse.status(500).send(`There was an error performing the statement: ${error}`)
+            })
     })
 }
